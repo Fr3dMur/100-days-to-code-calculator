3628d6c88d48ba26306e2df296e94cee1bf801af
Fr3dMur <murillo.freddy2002@gmail.com>
2023-03-26 15:31:58 -0400

    finish styles, starting js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const buttons = document.querySelectorAll("button");
+const arg1 = document.querySelector("arg1");
+const arg2 = document.querySelector("arg2");
+const screen = document.querySelector("screen")
 
