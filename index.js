f36050ec3854886ef86bf86d50dcce75f069beed
Fr3dMur <murillo.freddy2002@gmail.com>
2023-03-26 21:43:55 -0400

    finish a calculator just for plus two variables

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const arg1 = document.querySelector("arg1");
-const arg2 = document.querySelector("arg2");
-const screen = document.querySelector("screen")
+const arg2 = document.getElementById("arg2");
+const screen = document.getElementById("screen")
+
+// Function to plus result
+const plusValues = (a , b) => Number(a) + Number(b);
+
+// Function to get the value & see it on screen
+const getValue = () =>{
+    let arg1 = document.getElementById("arg1").value;
+    let arg2 = document.getElementById("arg2").value;
+
+    document.getElementById("screen").innerHTML = plusValues(arg1, arg2);
+}
 
