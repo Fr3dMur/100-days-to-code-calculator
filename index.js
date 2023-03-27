857587a81a2cf52190bb5f7f034da9ea94d2355d
Fr3dMur <murillo.freddy2002@gmail.com>
2023-03-27 19:25:00 -0400

    Ive added a few buttons, also ive created a function to make basic arithmetic

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ const arg2 = document.getElementById("arg2");
 const screen = document.getElementById("screen")
 
 // Function to plus result
-const plusValues = (a , b) => Number(a) + Number(b);
+// const plusValues = (a , b) => Number(a) + Number(b);
+
+// A simple calculator function when read that operation the user wants to use
+const Math = (a , b) => {
+    let sign = "/";
+    let result ;
+    switch (sign){
+        case "+" :
+            result = Number(a) + Number(b);
+            break;
+        case "-" :
+            result = a - b;
+            break;
+        case "*" :
+            result = Number(a) * Number(b);
+            break;
+        case "/" :
+            result = a / b;
+            break;
+        default :
+            result = "Give me a number"
+    }
+    return result;
+}
 
 // Function to get the value & see it on screen
 const getValue = () =>{
     let arg1 = document.getElementById("arg1").value;
     let arg2 = document.getElementById("arg2").value;
 
-    document.getElementById("screen").innerHTML = plusValues(arg1, arg2);
+    document.getElementById("screen").innerHTML = Math(arg1, arg2);
 }
 
