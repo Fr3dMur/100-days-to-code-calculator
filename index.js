2df1acc9c8402fb1c52345b8b5e24013fe309034
Fr3dMur <murillo.freddy2002@gmail.com>
2023-03-28 23:47:00 -0400

    I was trying to make a function that get the innerText in a button, but it isnt complete it

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const arg2 = document.getElementById("arg2");
 const screen = document.getElementById("screen")
 
 // Function to plus result
 // const plusValues = (a , b) => Number(a) + Number(b);
 
+// Get the symbol for arithmetic
+let arith ;
+
+const getSymbol = (buttonId) => {
+    let buttonContent;
+
+    switch (buttonId) {
+        case "arith1" :
+            buttonContent = document.getElementById("arith1");
+            console.log("El contenido del boton 1 es :" + buttonContent.innerHTML);
+            break;
+        case "arith2" :
+            buttonContent = document.getElementById("arith2");
+            console.log("El contenido del boton 2 es :" + buttonContent.innerHTML);
+            break;
+        case "arith3" :
+            buttonContent = document.getElementById("arith3");
+            console.log("El contenido del boton 3 es :" + buttonContent.innerHTML);
+            break;
+        case "arith4" :
+            buttonContent = document.getElementById("arith4");
+            console.log("El contenido del boton 4 es :" + buttonContent.innerHTML);
+            break;
+        default :
+            console.log("Dame un valor");
+    }
+};
+
 // A simple calculator function when read that operation the user wants to use
-const Math = (a , b) => {
-    let sign = "/";
+const Math = (a , b, c ) => {
     let result ;
-    switch (sign){
+
+    switch (c){
         case "+" :
             result = Number(a) + Number(b);
             break;
@@ -16,7 +43,7 @@ const Math = (a , b) => {
             result = a - b;
             break;
         case "*" :
-            result = Number(a) * Number(b);
+            result = a * b;
             break;
         case "/" :
             result = a / b;
@@ -24,6 +51,7 @@ const Math = (a , b) => {
         default :
             result = "Give me a number"
     }
+
     return result;
 }
 
@@ -32,6 +60,7 @@ const getValue = () =>{
     let arg1 = document.getElementById("arg1").value;
     let arg2 = document.getElementById("arg2").value;
 
-    document.getElementById("screen").innerHTML = Math(arg1, arg2);
+
+    document.getElementById("screen").innerHTML = Math(arg1, arg2, "*");
 }
 
